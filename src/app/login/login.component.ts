e34cb28b5c25fe21ac16ec7e71640b5b354b4875
juniorpires <juniorpiresupe@gmail.com>
2017-09-17 15:27:42 -0300

    adicionado lib login google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit } from '@angular/core';
+import { AuthService, AppGlobals } from 'angular2-google-login';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AuthService]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  constructor(private _googleAuth: AuthService) { }
 
   ngOnInit() {
+    AppGlobals.GOOGLE_CLIENT_ID = '449875052890-cdb1o95m4lo9ivtev74fio3p4trfvc0c.apps.googleusercontent.com';
   }
 
-  signOut() {
-    //var auth2 = gapi.auth2.getAuthInstance();
-    //auth2.signOut().then(function () {
-    //  console.log('User signed out.');
-    //});
+  ngAfterViewInit(){
+    
+  }
+
+  signIn() {
+    this._googleAuth.authenticateUser(()=>{
+      
+     });
   }
 
 }
